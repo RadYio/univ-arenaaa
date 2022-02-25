d468cbaafc1ccdc5b37f45112911620ff027a937
Gonin-Saget Allan <a193962@ic2s112-15.univ-lemans.fr>
2022-02-25 11:59:20 +0100

    menu terminé

--- src/window.c
+++ src/window.c
@@ -10,33 +10,44 @@
 
 
 //fonction de creation de fenetre, mise en place de tous les trucs importants de SDL (a documenter plus par la suite)
+/* @FUNC initialiser_fenetre()
+*
+*
+*
+*
+*
+*
+*
+*
+*/
 
 int initialiser_fenetre(){
   //initialisation de SDL---------------------------------------------------------------
-  if(SDL_Init(SDL_INIT_VIDEO) != 0 ) {
+  if(SDL_Init(SDL_INIT_VIDEO) != 0){
       fprintf(stdout,"Échec de l'initialisation de la SDL (%s)\n",SDL_GetError());
       return -1;
   }
 
     //creation de la fenetre-------------------------------------------------------------
-    SDL_Window * pWindow = NULL;
+    SDL_Window* pWindow = NULL;
     pWindow = SDL_CreateWindow(
       "UnivArena", //nom fenetre
       SDL_WINDOWPOS_CENTERED, //x fenetre centré
       SDL_WINDOWPOS_CENTERED, //y fenetre centré
       1600, //largeur fenetre
       900, //hauteur fenetre, deux valeurs à modifier potentiellement, a voir avec redimensionnement de la fenetre
-      SDL_WINDOW_SHOWN|SDL_WINDOW_RESIZABLE //fenetre redimensioable
+      SDL_WINDOW_SHOWN
     );
 
     if(pWindow){
       //lancement du jeu si fenetre bien crée-------------------------------------------------
       menu(pWindow); //on transmet le pointeur de la fenetre pour des utilisations antétieures
       SDL_DestroyWindow(pWindow);
-    }else{
+    }
+    else{
       printf("erreur de creation de fenetre\n");
       return -1;
     }
-    
+
   SDL_Quit();
 }
