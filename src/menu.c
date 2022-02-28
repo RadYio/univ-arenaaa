569c66c39beca89f226e52ced32246a66c658f03
Gonin-Saget Allan <a193962@ic2s130-08.univ-lemans.fr>
2022-02-28 17:18:30 +0100

    Fin du menu principal

--- src/menu.c
+++ src/menu.c
@@ -211,6 +211,8 @@ int menu(SDL_Window * pWindow){
 	recup_sauvegarde(tab_sauvegarde, tab_cartes_total);*/
 
 //gestion des evenements---------------------------------------------------------------------------------------------------------------------------------------------
+		//oldHover: Variable qui permet d'éviter de render toutes les frames, si pas besoin
+		int oldHover=0;
 
 if(pWindow){
   int running = 1;
@@ -233,37 +235,86 @@ if(pWindow){
 
               SDL_RenderCopy(renderer_menu, img_Menu_Texture, NULL, NULL);
               SDL_RenderCopy(renderer_menu, img_Choix_Texture, NULL, NULL);
-
 							//Render de toutes les options du menu principal
 							//OPTION 1
 							SDL_RenderCopy(renderer_menu, txt_optn1_T, NULL, &txt_optn1_R);
-							SDL_RenderCopy(renderer_menu, txt_optn1_Hover_T, NULL, &txt_optn1_R);
+
 							//OPTION 2
 							SDL_RenderCopy(renderer_menu, txt_optn2_T, NULL, &txt_optn2_R);
-							SDL_RenderCopy(renderer_menu, txt_optn2_Hover_T, NULL, &txt_optn2_R);
 							//OPTION 3
 							SDL_RenderCopy(renderer_menu, txt_optn3_T, NULL, &txt_optn3_R);
-							SDL_RenderCopy(renderer_menu, txt_optn3_Hover_T, NULL, &txt_optn3_R);
 							//OPTION 4
 							SDL_RenderCopy(renderer_menu, txt_optn4_T, NULL, &txt_optn4_R);
-							SDL_RenderCopy(renderer_menu, txt_optn4_Hover_T, NULL, &txt_optn4_R);
-
-
-
-
-              /* On fait le rendu ! */
+              //On fait le rendu !
               SDL_RenderPresent(renderer_menu);
 
             break;
           }
         case SDL_MOUSEBUTTONDOWN:
-
 					printf("x: %i\ny: %i\n",e.button.x,e.button.y);
-          if(e.button.x>=	button1.x && e.button.x<=button1.x+button1.largeur && e.button.y>=button1.y && e.button.y<=button1.y+button1.hauteur){
-            //SDL_DestroyTexture(img_Choix_Texture);
-						printf("T BG\n");
+          if(e.button.x >= txt_optn1_R.x && e.button.x <= txt_optn1_R.x+txt_optn1_R.w && e.button.y >= txt_optn1_R.y && e.button.y <= txt_optn1_R.y+txt_optn1_R.h){
+            //Si on clique sur le bouton 1
+
           }
-        break;
+					case SDL_MOUSEMOTION:
+						//SOURIS QUI HOVER SUR LE BOUTON 1
+						if(e.button.x >= txt_optn1_R.x && e.button.x <= txt_optn1_R.x+txt_optn1_R.w && e.button.y >= txt_optn1_R.y && e.button.y <= txt_optn1_R.y+txt_optn1_R.h){
+							SDL_RenderCopy(renderer_menu, txt_optn1_Hover_T, NULL, &txt_optn1_R);
+							SDL_RenderPresent(renderer_menu);
+							//oldHover: Variable qui permet d'éviter de render toutes les frames, si pas besoin
+							oldHover=1;
+						}
+						//SOURIS QUI HOVER SUR LE BOUTON 2
+						else if(e.button.x >= txt_optn2_R.x && e.button.x <= txt_optn2_R.x+txt_optn2_R.w && e.button.y >= txt_optn2_R.y && e.button.y <= txt_optn2_R.y+txt_optn2_R.h){
+							SDL_RenderCopy(renderer_menu, txt_optn2_Hover_T, NULL, &txt_optn2_R);
+							SDL_RenderPresent(renderer_menu);
+							//oldHover: Variable qui permet d'éviter de render toutes les frames, si pas besoin
+							oldHover=1;
+						}
+						//SOURIS QUI HOVER SUR LE BOUTON 3
+						else if(e.button.x >= txt_optn3_R.x && e.button.x <= txt_optn3_R.x+txt_optn3_R.w && e.button.y >= txt_optn3_R.y && e.button.y <= txt_optn3_R.y+txt_optn3_R.h){
+							SDL_RenderCopy(renderer_menu, txt_optn3_Hover_T, NULL, &txt_optn3_R);
+							SDL_RenderPresent(renderer_menu);
+							//oldHover: Variable qui permet d'éviter de render toutes les frames, si pas besoin
+							oldHover=1;
+						}
+						//SOURIS QUI HOVER SUR LE BOUTON 4
+						else if(e.button.x >= txt_optn4_R.x && e.button.x <= txt_optn4_R.x+txt_optn4_R.w && e.button.y >= txt_optn4_R.y && e.button.y <= txt_optn4_R.y+txt_optn4_R.h){
+							// On clear le render, et on raffiche tout ici, car on modifie le texte du quitter qui devient noooo, sinon même chose que les autres detections 1/2/3
+							SDL_RenderClear(renderer_menu);
+							SDL_RenderCopy(renderer_menu, img_Menu_Texture, NULL, NULL);
+							SDL_RenderCopy(renderer_menu, img_Choix_Texture, NULL, NULL);
+							SDL_RenderCopy(renderer_menu, txt_optn1_T, NULL, &txt_optn1_R);
+							SDL_RenderCopy(renderer_menu, txt_optn2_T, NULL, &txt_optn2_R);
+							SDL_RenderCopy(renderer_menu, txt_optn3_T, NULL, &txt_optn3_R);
+							SDL_RenderCopy(renderer_menu, txt_optn4_Hover_T, NULL, &txt_optn4_R);
+							//On fait le rendu !
+							SDL_RenderPresent(renderer_menu);
+							//oldHover: Variable qui permet d'éviter de render toutes les frames, si pas besoin
+							oldHover=1;
+						}
+						//Si on a deja effectué des hovers
+						else if(oldHover){
+								SDL_RenderClear(renderer_menu);
+								SDL_RenderCopy(renderer_menu, img_Menu_Texture, NULL, NULL);
+	              SDL_RenderCopy(renderer_menu, img_Choix_Texture, NULL, NULL);
+								//Render de toutes les options du menu principal
+								//OPTION 1
+								SDL_RenderCopy(renderer_menu, txt_optn1_T, NULL, &txt_optn1_R);
+
+								//OPTION 2
+								SDL_RenderCopy(renderer_menu, txt_optn2_T, NULL, &txt_optn2_R);
+								//OPTION 3
+								SDL_RenderCopy(renderer_menu, txt_optn3_T, NULL, &txt_optn3_R);
+								//OPTION 4
+								SDL_RenderCopy(renderer_menu, txt_optn4_T, NULL, &txt_optn4_R);
+	              //On fait le rendu !
+	              SDL_RenderPresent(renderer_menu);
+								//oldHover: Variable qui permet d'éviter de render toutes les frames, si pas besoin
+								oldHover=0;
+								printf("Reset actuel\n\n\n");
+						}
+        	break;
       }
     }
   }
