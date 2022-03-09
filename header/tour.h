a30a0ffe73fe5b1aca1dbbcee2dfa154fdb9ea3c
Johno <Jonathrsl@gmail.com>
2022-03-09 23:38:34 +0100

    ajout musique

--- header/tour.h
+++ header/tour.h
@@ -1,4 +1,4 @@
-void choix_plateau(SDL_Renderer* renderer_jeu, SDL_Window* pWindow, SDL_Rect tab_rect_main[12], int tab_formation_cartesJ[5][3], SDL_Rect tab_rect_formationJ[15]);
+void choix_plateau(SDL_Renderer* renderer_jeu, SDL_Window* pWindow, SDL_Rect tab_rect_main, int tab_formation_cartesJ[5][3], SDL_Rect tab_rect_formationJ[15], int deck_main[12], int indice_main);
 
 
 int tour(SDL_Renderer* renderer_jeu, SDL_Window* pWindow, SDL_Rect tab_rect_main[12], int tab_formation_cartesJ[5][3], SDL_Rect tab_rect_formationJ[15]);
