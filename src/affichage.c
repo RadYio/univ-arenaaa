75ba6273027c7296c01d940fda986f292cb0b7bd
Johno <Jonathrsl@gmail.com>
2022-03-29 18:08:15 +0200

    doxygenation de affichage.c

--- src/affichage.c
+++ src/affichage.c
@@ -1,3 +1,11 @@
+/**
+ * @file affichage.c
+ * @author Jonathan Otto et Arthur Boullier
+ * @brief fichier contenant les fonctions d'affichage du jeu ainsi que certaines fonctions essentielles à la mise à jour de tableaux qui seront affichés ensuite
+ * @version 1
+ * @date 2022-03-28
+ */
+
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -18,7 +26,14 @@
 
 
 
-//fonction d'affichage du background
+/**
+ * @fn void affichage_BG(SDL_Renderer* renderer_jeu, SDL_Texture* img_jeu_Texture)
+ * @brief fonction d'affichage du fond lors du jeu
+ * @param SDL_Renderer* renderer_jeu notre renderer
+ * @param SDL_Texture* img_jeu_Texture la texture contenant l'image à afficher
+ *
+ * @return void
+ */
 void affichage_BG(SDL_Renderer* renderer_jeu, SDL_Texture* img_jeu_Texture){
   // Le fond de la fenêtre sera blanc
   SDL_SetRenderDrawColor(renderer_jeu, 255, 255, 255, 255);
@@ -31,6 +46,18 @@ void affichage_energie(int * nb_actions){
 
 }
 
+
+/**
+ * @fn void affichage_gros_rectangles(SDL_Renderer* renderer_jeu, SDL_Rect* rect_joueur,SDL_Rect* rect_adv,SDL_Rect * rect_aff_carte_j)
+ *
+ * @brief fonction d'affichage des grands rectangles qui séparent le coté joueur du coté adversaire
+ * @param SDL_Renderer* renderer_jeu notre renderer
+ * @param SDL_Rect* rect_joueur le grand rectangle du joueur
+ * @param SDL_Rect* rect_adv le grand rectangle de l'adversaire
+ * @param SDL_Rect* rect_aff_carte_j rectangle d'affichage pour la zone d'affichage en gros d'une carte
+ *
+ * @return void
+ */
 void affichage_gros_rectangles(SDL_Renderer* renderer_jeu, SDL_Rect* rect_joueur,SDL_Rect* rect_adv,SDL_Rect * rect_aff_carte_j){
   //creation de rectangle du joueur
   SDL_SetRenderDrawColor( renderer_jeu, 0, 213, 255, 255 ); //renderer passe en bleu pour le rectangle de la zone joueur
@@ -40,11 +67,20 @@ void affichage_gros_rectangles(SDL_Renderer* renderer_jeu, SDL_Rect* rect_joueur
   SDL_SetRenderDrawColor( renderer_jeu, 219, 0, 0, 255 ); //renderer passe en rouge pour le rectangle de la zone adversaire
   SDL_RenderDrawRect(renderer_jeu, rect_adv);
 
-  SDL_SetRenderDrawColor( renderer_jeu, 0, 219, 0, 255 ); //renderer passe en vert pour le rectangle de la zone adversaire
+  SDL_SetRenderDrawColor( renderer_jeu, 0, 219, 0, 255 ); //renderer passe en vert pour le rectangle de la zone d'affichage d'une carte en particulier
   SDL_RenderDrawRect(renderer_jeu, rect_aff_carte_j);
 }
 
-//fonction d'affichage des rectangles de la main
+
+/**
+ * @fn void afficher_rectangles_main(int * taille_main,SDL_Renderer* renderer_jeu, SDL_Rect tab_rect_main[12])
+ * @brief fonction d'affichage des rectangles autour des cartes dans la main du joueur
+ * @param int* taille_main variable qui repertorie la taille de la main, permet un affichage dynamique en fonction du nombre de carte
+ * @param SDL_Renderer* renderer_jeu notre renderer
+ * @param SDL_Rect tab_rect_main[] repertorie les rectangles de la main du joueur
+ *
+ * @return void
+ */
 void afficher_rectangles_main(int * taille_main,SDL_Renderer* renderer_jeu, SDL_Rect tab_rect_main[12]){
   SDL_SetRenderDrawColor(renderer_jeu, 255, 255, 255, 255); //on passe en jaune
   for(int i = 0; i < *taille_main; i++){
@@ -66,14 +102,21 @@ void afficher_boutton(SDL_Rect* rect_menu,SDL_Rect* rect_passe, SDL_Renderer* re
 
   SDL_FreeSurface(menu_s);
   SDL_FreeSurface(passe_s);
-
-
-
 }
 
 
-//affiche les cartes qu'il y a dans la main du joueur != cartes sur le plateau
-void affichage_main(int * taille_main,SDL_Renderer* renderer_jeu, carte_t tab_main[], SDL_Rect tab_rect_main[]){
+
+/**
+ * @fn void affichage_main(int * taille_main,SDL_Renderer* renderer_jeu, carte_t tab_main[], SDL_Rect tab_rect_main[])
+ * @brief fonction d'affichage des cartes de la main du joueur
+ * @param int* taille_main variable qui repertorie la taille de la main, permet un affichage dynamique en fonction du nombre de cartes
+ * @param SDL_Renderer* renderer_jeu notre renderer
+ * @param carte_t tab_main[] tableau qui repertorie quelles cartes possède le joueur dans sa main
+ * @param SDL_Rect tab_rect_main[] tableau qui repertorie les rectangles de la main pour savoir où afficher les cartes
+ *
+ * @return void
+ */
+ void affichage_main(int * taille_main,SDL_Renderer* renderer_jeu, carte_t tab_main[], SDL_Rect tab_rect_main[]){
     int i;
     for(i = 0; i < *taille_main; i++){
       affichage_carte(renderer_jeu, tab_main[i], tab_rect_main[i]);
@@ -81,7 +124,15 @@ void affichage_main(int * taille_main,SDL_Renderer* renderer_jeu, carte_t tab_ma
 }
 
 
-
+/**
+ * @fn void affichage_carte(SDL_Renderer* renderer_jeu,carte_t carte,SDL_Rect rect_afficher)
+ * @brief fonction d'affichage d'une carte donnée dans un rectangle donnée
+ * @param SDL_Renderer* renderer_jeu notre renderer
+ * @param carte_t carte la carte à afficher
+ * @param SDL_Rect rect_afficher le rectangle où afficher la carte en question
+ *
+ * @return void
+ */
 void affichage_carte(SDL_Renderer* renderer_jeu,carte_t carte,SDL_Rect rect_afficher){
   SDL_Surface* carte_s;
   SDL_Texture* cartes_T;
@@ -92,8 +143,17 @@ void affichage_carte(SDL_Renderer* renderer_jeu,carte_t carte,SDL_Rect rect_affi
   SDL_DestroyTexture(cartes_T);
 }
 
-
-//fonction d'affichage des rectangles de formation
+/**
+ * @fn void afficher_rectangles_formation(SDL_Renderer* renderer_jeu,carte_t tab_cartes_total[],int tab_formation_cartesJ[5][3], SDL_Rect tab_rect_formationJ[][3], int tab_formation_cartesADV[5][3], SDL_Rect tab_rect_formationAdv[][3])
+ * @brief fonction d'affichage des rectangles de la formationd du joueur en parcourant la matrice associé et en mettant un rectangle lorsqu'il y à un -2
+ * @param SDL_Renderer* renderer_jeu notre renderer
+ * @param carte_t tab_cartes_total[] tableau qui repertorie toutes les cartes du jeu, permet de faire corresponndre une id de carte que possède le joueur aux reste des informations de la carte
+ * @param int tab_formation_cartesJ[5][3] tableau correspondant à la formation du joueur, -2 correspond à un emplacement où l'on peut poser une carte, -1 est un emplacement où on à rien, et tout chiffre >= 0 est une carte
+ * @param SDL_Rect tab_rect_formationJ[][3] tableau correspondant aux rectangles de la formation du joueur, permet de connaitre où on pose les cartes
+ * @param int tab_formation_cartesADV[5][3] tableau qui reprend le meme principe que tab_formation_cartesJ[][3] mais pour le coté adversaire
+ * @param SDL_Rect tab_rect_formationAdv[][3] tableau qui reprend le meme principe que tab_rect_formationJ[][3] mais pour le coté adversaire
+ * @return void
+ */
 void afficher_rectangles_formation(SDL_Renderer* renderer_jeu,carte_t tab_cartes_total[],int tab_formation_cartesJ[5][3], SDL_Rect tab_rect_formationJ[][3], int tab_formation_cartesADV[5][3], SDL_Rect tab_rect_formationAdv[][3]){
   //creation des rectangles pour les lieux de placement des cartes
     SDL_SetRenderDrawColor(renderer_jeu, 255, 215, 0, 255); //renderer passe en jaune pour ces rectangles
@@ -221,14 +281,32 @@ SDL_Rect txt_menu_R,SDL_Texture *txt_menu_T,SDL_Texture *passe_t,SDL_Rect passe_
     SDL_RenderPresent(renderer_jeu);
 }
 
-//a mettre dans fichier séparé
-void supprimer_carte_tab(carte_t tab_main[], int indice_main, int* taille_main){
+
+/**
+ * @fn void supprimer_carte_tab(carte_t tab_main[], int indice_main, int* taille_main)
+ * @brief fonction de suppression d'une carte dans un tableau
+ * @param carte_t tab_main[] le tableau qui contient l'élément à supprimer
+ * @param int indice_main l'indice de l'élément à supprimer dans le tableau
+ * @param int* taille_main taille du tableau
+ *
+ * @return void
+ */
+ void supprimer_carte_tab(carte_t tab_main[], int indice_main, int* taille_main){
   for(int i = indice_main ; i < *taille_main; i++){
     tab_main[i] = tab_main[i+1];
   }
 }
 
-//a mettre dans fichier séparé
+/**
+ * @fn void suppression_carte_main(carte_t tab_main[], int indice_main, SDL_Rect tab_rect_main[], int* taille_main)
+ * @brief fonction de suppression d'une carte dans la main du joueur
+ * @param carte_t tab_main[] tableau de la main où il faut supprimer un élément
+ * @param int indice_main indice de l'élément à supprimer dans le tableau
+ * @param SDL_Rect tab_rect_main[] tableau de la main du joueur qui possède un élément à supprimer
+ * @param int* taille_main la taille de la main qui devra être mis à jour après avoir supprimé un élément
+ *
+ * @return void
+ */
 void suppression_carte_main(carte_t tab_main[], int indice_main, SDL_Rect tab_rect_main[], int* taille_main){
   //suppression de la carte dans tab_main
   (*taille_main)--;
@@ -238,9 +316,19 @@ void suppression_carte_main(carte_t tab_main[], int indice_main, SDL_Rect tab_re
 }
 
 
-
-//fonction qui tranfère une carte de la main du joueur sur le plateau                       //MAJ               //MAJ
-void transfert_carte(carte_t tab_main[], int tab_formation_cartesJ[5][3], SDL_Rect tab_rect_main[], int colone, int ligne, int indice_main, int* taille_main){
+/**
+ * @fn void transfert_carte(carte_t tab_main[], int tab_formation_cartesJ[5][3], SDL_Rect tab_rect_main[], int colone, int ligne, int indice_main, int* taille_main)
+ * @brief fonction qui transfère une carte de la main du joueur sur le plateau
+ * @param carte_t tab_main[] tableau de la main dont il faut transférer une carte
+ * @param int tab_formation_cartesJ[5][3] tableau qui répértorie où on peut poser une carte, sera mis à jour avec l'id de la carte nouvellement placé à l'endroit voulu
+ * @param SDL_Rect tab_rect_main[] tableau des rectangles de la main, sert à savoir sur quelle carte de la main on à cliqué
+ * @param int colone sert de coordonnée dans la formation
+ * @param int ligne sert de coordonnée dans la formation
+ * @param int* taille_main taille de la main qui doit être mis à jour
+ *
+ * @return void
+ */
+ void transfert_carte(carte_t tab_main[], int tab_formation_cartesJ[5][3], SDL_Rect tab_rect_main[], int colone, int ligne, int indice_main, int* taille_main){
   //mises à jour des tableaux
   if(tab_formation_cartesJ[colone][ligne] == -2){ //si on peut placer une carte (-2 représente un endroit où on peut)
     //mise à jour de notre formation,on remplace le -2 par l'id de la carte
