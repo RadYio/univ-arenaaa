f835493a595c72db69eaed0f76545ce981160e74
RadYio <allan.gonin-saget.etu@univ-lemans.fr>
2022-03-30 18:17:20 +0200

    hello back again

--- src/jeu_multi.c
+++ src/jeu_multi.c
@@ -343,8 +343,8 @@ int tab_formation_cartesADV[5][3] = { //ceci est le tableau de l'adversaire
                   tab_cartes_deck_adv[i] = etatDuJeu.tab1[i];
                   tab_cartes_deck[i] = etatDuJeu.tab2[i];
                 }
-                etatDuJeu.flag= 0;
-
+                etatDuJeu.flag=0;
+                printf("Je devrais afficher\n\n");
                 affichage_jeu2 (renderer_jeu,img_jeu_Texture,rect_aff_carte_j, rect_txt_deck_j,txt_titre_joueur_T,rect_txt_deck_adv,txt_titre_adv_T,rect_joueur,
                     rect_adv, tab_formation_cartesJ, tab_rect_formationJ,tab_formation_cartesADV,tab_rect_formationAdv ,taille_main, tab_rect_main, tab_main,tab_cartes_total,
                     menu_t,menu_R,txt_menu_Hover_T,txt_menu_R,txt_menu_T,passe_t,passe_R,txt_passe_Hover_T,txt_passe_T,txt_passe_R,nb_actions);
