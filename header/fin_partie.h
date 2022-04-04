597905117c44f9216d6270026f15fde4084d5b2c
Otto Jonathan <s203750@ic2s114-02.univ-lemans.fr>
2022-04-04 16:42:35 +0200

    debug

--- header/fin_partie.h
+++
@@ -1,5 +0,0 @@
-void victoire(SDL_Renderer* renderer_jeu);
-
-void defaite(SDL_Renderer* renderer_jeu);
-
-void affichage_victoire(SDL_Renderer* renderer_jeu, SDL_Surface* txt);
--- header/init_jeu_solo.h
+++
@@ -1,9 +0,0 @@
-
-
-SDL_Rect creer_rectangle(int x, int y, int h, int w);
-
-void init_jeu(int  * taille_main,SDL_Rect tab_rect_formationJ[][3], SDL_Rect tab_rect_formationAdv[][3], SDL_Rect tab_rect_main[12],
- SDL_Rect* rect_joueur, SDL_Rect* rect_adv, SDL_Rect* rect_txt_deck_j, SDL_Rect* rect_txt_deck_adv,SDL_Rect* rect_aff_carte_j,
- SDL_Rect* rect_aff_att_j ,SDL_Rect* rect_aff_hp_j);
- 
-void creation_main(int  *taille_main,SDL_Rect tab[]);
